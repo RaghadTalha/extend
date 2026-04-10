b4fe67bd4573f7f3b2a3cde17cee19803dc8b118
RaghadTalha <Raghadtalha@gmail.com>
2026-04-09 22:49:35 -0400

    Add files via upload

--- research.pl
+++ research.pl
@@ -1,1 +1,37 @@
+% CMPSC 360 - Spring 2026
+% Problem 2: Research Circle Problem
+%
+% Use this file to declare the knowledge base and rules
+% for the research interests and collaborations described
+% in the project PDF and p2.txt.
+%
+% IMPORTANT:
+% - Copy the information from p2.txt into facts here.
+% - Do NOT import the knowledge base from another file.
+
+% Declare the facts here
+
+
+
+% helper rule for shared interest
+shared_interest(X, Y) :-
+    % fill logic
+    .
+
+
+% helper rule for shared collaborator
+shared_collaborator(X, Y) :-
+    % fill logic
+    .
+
+
+% Complete the following horn clause.
+% An individual X is a research circle member if they
+% share a research interest with someone other than
+% themselves AND share a common collaborator with
+% someone other than themselves.
+
+research_circle_member(X) :-
+    % write logic
+    .
 
