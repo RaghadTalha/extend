868d8dfef54bac9ec82ab2243e5e5dba435d4501
RaghadTalha <Raghadtalha@gmail.com>
2026-04-09 22:42:21 -0400

    extend.pl

--- extend.pl
+++ extend.pl
@@ -1,19 +1,3 @@
-% CMPSC 360 - Spring 2026
-% Problem 1: Tree Extension Problem
-%
-% Use this file to declare the knowledge base and rules
-% for the tree extension relationships described in the
-% project PDF.
-%
-% IMPORTANT:
-% - Declare all of your facts directly in this file.
-% - Do NOT import the knowledge base from another file.
-%
-% Then complete the definition of extends_to/2 so that it
-% succeeds when there is a (possibly multi-step) extension
-% path from X to Y.
-
-% Declare your facts here
 extends(oak, maple).
 extends(oak, pine).
 extends(maple, birch).
@@ -30,13 +14,10 @@ extends(palm, coconut).
 extends(palm, date).
 
 
-% Complete the following horn clause(s)
 
-% Base case: direct extension
 extends_to(X, Y) :-
     extends(X, Y).
 
-% Recursive case: transitive extension
 extends_to(X, Y) :-
     extends(X, Z),
     extends_to(Z, Y).
